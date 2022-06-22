da57dd10eb4f0307296b4b318a3243cb061b7ba8
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-06-22 19:16:00 +0100

    unique list passed; homework done

--- models/traveller.js
+++ models/traveller.js
@@ -32,6 +32,14 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
   }, 0);
 };
 
-Traveller.prototype.getUniqueModesOfTransport = function () {};
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  return this.journeys
+    .map((journey) => {
+      return journey.transport;
+    })
+    .filter((transport, index, array) => {
+      return array.indexOf(transport) === index;
+    });
+};
 
 module.exports = Traveller;
